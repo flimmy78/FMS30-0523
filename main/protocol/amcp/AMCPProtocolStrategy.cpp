968fa052c55df4864c5f1efe3c75baf77eec9c46
seabeach000 <x.g.wang@qq.com>
2017-06-01 11:14:31 +0800

    #568 Fixed bug where not all error responses was preprended with RES [requestId]

--- main/protocol/amcp/AMCPProtocolStrategy.cpp
+++ main/protocol/amcp/AMCPProtocolStrategy.cpp
@@ -98,6 +98,7 @@ public:
 	struct command_interpreter_result
 	{
 		std::shared_ptr<caspar::IO::lock_container>	lock;
+		std::wstring								request_id;
 		std::wstring								command_name;
 		AMCPCommand::ptr_type						command;
 		error_state									error			= error_state::no_error;
@@ -246,6 +247,9 @@ public:
 		{
 			std::wstringstream answer;
 
+			if (!result.request_id.empty())
+				answer << L"RES " << result.request_id << L" ";
+
 			switch(result.error)
 			{
 			case error_state::command_error:
@@ -283,7 +287,6 @@ private:
 			if (!tokens.empty() && tokens.front().at(0) == L'/')
 				tokens.pop_front();
 
-			std::wstring request_id;
 
 			if (!tokens.empty() && boost::iequals(tokens.front(), L"REQ"))
 			{
@@ -295,7 +298,7 @@ private:
 					return false;
 				}
 
-				request_id = tokens.front();
+				result.request_id = tokens.front();
 				tokens.pop_front();
 			}
 
@@ -396,7 +399,7 @@ private:
 			}
 
 			if (result.command)
-				result.command->set_request_id(std::move(request_id));
+				result.command->set_request_id(std::move(result.request_id));
 		}
 		catch (std::out_of_range&)
 		{
